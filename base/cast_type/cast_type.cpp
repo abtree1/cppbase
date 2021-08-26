9618b4cd2d724d977f90adedeba1fa9630e05975
abtree <abtree@qq.com>
2021-08-26 16:52:49 +0800

    update

--- base/cast_type/cast_type.cpp
+++ base/cast_type/cast_type.cpp
@@ -18,6 +18,12 @@ int main(){
 	ch.show();
 
 	char *pc = reinterpret_cast<char *>(l);   //位模式强制转换，将long型按位转换为对应的char串
+	int addr_l = reinterpret_cast<int>(&l);	//取出l的地址 并保存为int
+	/* link_l的地址与l一样，因此link_l == l
+	   修改link_l也会修改l
+	   这一步的操作就等于 long* link_l = &l;
+	*/
+	long* link_l = reinterpret_cast<long*>(addr_l);	//
 	return 0;
 }
 
